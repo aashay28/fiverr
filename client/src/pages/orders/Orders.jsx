752375c3e66ba443ff195a109c851179bc750e12
aashay shah <aashayshah28@gmail.com>
2023-04-11 19:04:22 +0530

    Updated Order Api and frontend side

--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Orders.scss";
-
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 const Orders = () => {
-  const currentUser = {
-    id: 1,
-    username: "Anna",
-    isSeller: true,
-  };
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () =>
+      newRequest.get(`/orders`).then((res) => {
+        return res.data;
+      }),
+  });
 
   return (
-    <div className="orders">
-      <div className="container">
-        <div className="title">
+    <div className='orders'>
+      <div className='container'>
+        <div className='title'>
           <h1>Orders</h1>
         </div>
         <table>
@@ -23,96 +27,42 @@ const Orders = () => {
             {<th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>}
             <th>Contact</th>
           </tr>
-          <tr>
+          {isLoading
+            ? "loading"
+            : error
+            ? "error"
+            : data.map((order) => (
+                <tr key={order._id}>
+                  <td>
+                    <img className='image' src={order.img} alt='' />
+                  </td>
+                  <td>{order.title}</td>
+                  <td>{order.price}</td>
+                  <td>
+                    {currentUser.isSeller ? order.buyerId : order.sellerId}
+                  </td>
+                  <td>
+                    <img className='message' src='./img/message.png' alt='' />
+                  </td>
+                </tr>
+              ))}
+          {/* <tr>
             <td>
               <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
+                className='image'
+                src='https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600'
+                alt=''
               />
             </td>
             <td>Stunning concept art</td>
-            <td>59.<sup>99</sup></td>
+            <td>
+              59.<sup>99</sup>
+            </td>
             <td>Maria Anders</td>
             <td>
-              <img className="message" src="./img/message.png" alt="" />
+              <img className='message' src='./img/message.png' alt='' />
             </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Ai generated concept art</td>
-            <td>79.<sup>99</sup></td>
-            <td>Francisco Chang</td>
-            <td>
-              <img className="message" src="./img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>High quality digital character</td>
-            <td>110.<sup>99</sup></td>
-            <td>Roland Mendel</td>
-            <td>
-              <img className="message" src="./img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Illustration hyper realistic painting</td>
-            <td>39.<sup>99</sup></td>
-            <td>Helen Bennett</td>
-            <td>
-              <img className="message" src="./img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Original ai generated digital art</td>
-            <td>119.<sup>99</sup></td>
-            <td>Yoshi Tannamuri</td>
-            <td>
-              <img className="message" src="./img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Text based ai generated art</td>
-            <td>49.<sup>99</sup></td>
-            <td>Giovanni Rovelli</td>
-            <td>
-              <img className="message" src="./img/message.png" alt="" />
-            </td>
-          </tr>
+          </tr> */}
         </table>
       </div>
     </div>
