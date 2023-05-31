569658bb07ae6c007fced204543b77a1ad6a12bf
aashay shah <aashayshah28@gmail.com>
2023-05-31 12:43:40 +0530

    created get all users api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,24 +1,39 @@
-const User = require("../models/user.model.js");
-const jwt = require("jsonwebtoken");
-const createError = require("../utils/createError.js");
+const User = require('../models/user.model.js');
+const jwt = require('jsonwebtoken');
+const createError = require('../utils/createError.js');
 exports.deleteUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
     if (req.userId !== user._id.toString()) {
-      return next(createError(403, "You can only delete your account"));
+      return next(createError(403, 'You can only delete your account'));
     }
     await User.findByIdAndDelete(req.params.id);
-    return res.status(200).send("Deleted");
+    return res.status(200).send('Deleted');
+  } catch (err) {
+    next(err);
+  }
+};
+exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select('-password');
+
+    if (!users) {
+      return next(createError(403, 'Users not found'));
+    }
+
+    return res.status(200).send(users);
   } catch (err) {
     next(err);
   }
 };
 exports.getUser = async (req, res, next) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id).select('-password');
+
     if (!user) {
-      return next(createError(403, "User not found"));
+      return next(createError(403, 'User not found'));
     }
+
     return res.status(200).send(user);
   } catch (err) {
     next(err);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const { deleteUser, getUser } = require("../controllers/user.controller.js");
-const { verifyToken } = require("../middleware/jwt.js");
+const express = require('express');
+const {
+  deleteUser,
+  getUser,
+  getUsers,
+} = require('../controllers/user.controller.js');
+const { verifyToken } = require('../middleware/jwt.js');
 const router = express.Router();
 
-router.delete("/:id", verifyToken, deleteUser);
-router.get("/:id", getUser);
+router.delete('/:id', verifyToken, deleteUser);
+router.get('/:id', getUser);
+router.get('/', getUsers);
 module.exports = router;
